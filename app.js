9da9c7f8ce952bddbd7a6db485c66fb9dfdc3525
JulioZapata <julio.cesar.zaor@gmail.com>
2024-03-10 20:08:41 -0500

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World, Julio Zapata!'));
+app.get('/', (req, res) => res.send('Hello World, Julio Zapata!. Ahora este cambio es para probar un review stage del pipeline'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
